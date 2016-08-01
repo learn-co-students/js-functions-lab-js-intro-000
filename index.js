683093f7db1c19b2702f2a5839842bdca10dd518
JMixson1179 <jmixson1179@gmail.com>
2016-07-31 23:05:21 -0700

    Removing function calls with assigned parameters
    The function calls at the end of the functions weren't needed because they are being called in the test/index-test.js file.

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
-happyHolidaysTo("you");
+
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
-happyHolidayTo("Independence Day", "you");
+
 
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
-holidayCountdown("Mother's Day", 20);
+
