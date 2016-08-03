27a8999263ccdf1f4b8e9dc6a6eb47d9ce3eb141
Steven Covington <dlinetackle@yahoo.com>
2016-08-03 15:47:24 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays() {
   return 'Happy holidays!';
 }
 function happyHolidaysTo(name) {
-  return `Happy holidays,${name}!`;
+  return `Happy holidays, ${name}!`;
   }
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
