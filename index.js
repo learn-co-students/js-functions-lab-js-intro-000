9603da9bec36eabd1af9311a81a7aa74c9aff5b6
David Ganthier <david.ganthier@gmail.com>
2016-08-12 01:30:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays() {
+  return("Happy holidays!");
+}
+
+function happyHolidaysTo(name) {
+  return("Happy Holidays, ${name}!");
+  var name = "you";
+}
+
+function happyHolidayTo(holiday, name) {
+  return("Happy ${holiday}, ${name}!");
+  var holiday = "Independence Days";
+  var name = "David";
+}
+
+function holidayCountdown(holiday, days) {
+  return("It's ${days} days until #{holiday}!");
+  var holiday = "Mother's Day";
+  var days = "20";
+}
