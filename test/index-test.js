9eff6f56e10ae90f8fb9a049997df0020366b69f
Carrie Jeanne <cdonica@ufl.edu>
2016-08-16 22:23:49 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,27 +1,17 @@
 /*global describe, it*/
 
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
+function happyHolidays () {
+  return "Happy holidays!"
+}
 
-  describe('happyHolidaysTo(name)', () => {
-    it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-    })
-  })
+function happyHolidayTo(name) {
+  return "Happy holidays, ${name}!"
+}
 
-  describe('happyHolidayTo(holiday, name)', () => {
-    it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
-    })
-  })
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
 
-  describe('holidayCountdown(holiday, days)', () => {
-    it('returns "It\'s ${days} until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-    })
-  })
-})
+function holidayCountdown(holiday, days) {
+  return "It/'s ${days} until ${holiday}!'"
+}
