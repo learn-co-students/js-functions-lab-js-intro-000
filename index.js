f3b1a5464b296369344904c38eca94d585e2737d
pwjablonski Jablonski <pwjablonski@gmail.com>
2016-08-18 19:29:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}// write your code below!
+
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, " + name + "!";
+}// write your code below!
+
+function happyHolidayTo(holiday, name) {
+  return "Happy " + holiday+ ", " + name + "!";
+}// write your code below!
+
+function holidayCountdown(holiday, days) {
+  return "It\'s " + days.toString() + " days until " + holiday + "!";
+}//
