ec88cdb663bf36374f74f2e027a2890ddfd374db
Devy Js <devyjs77@gmail.com>
2016-08-27 07:24:30 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 function happyHolidaysTo(name) {
-  return (Happy holidays, ${name}!)
+  return (`Happy holidays, ${name}!`)
 }
 function happyHolidaysTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
