e3a31dec998f5ed753c200b54bb609135afb89c7
Zaynah Shaikh <zaynahshaikh11@gmail.com>
2016-10-18 03:59:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+var string = "Happy holidays!";
+var name = "Zaynah";
+var holiday = "Independence Day";
+
+function happyHolidays() {
+  return string;
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!";
+}
+
+function happyHolidayTo(holiday,name) {
+  return "Happy Independence Day, you!";
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's 20 days until Mother's Day!";
+}
