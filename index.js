4327aec0687e42e7ac3e66067be3a4bc085b52b6
Raissa Hughes <skurredkid@gmail.com>
2016-11-11 03:41:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(Raissa) {
+  return `Happy holidays, ${Raissa}!`;
+}
+
+function happyHolidayTo(Independance, Raissa) {
+  return `Happy ${Independance}, ${Raissa}!`;
+}
+
+function holidayCountdown(Independance, sixteen) {
+  return `It's ${sixteen} days until ${Independance}!` 
+}
