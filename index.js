8f7eeaf9dfc98500a1d640d08e5a16af1b553128
Brandon Tyus <ausis@mail.com>
2016-12-13 00:35:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+var name = "Brandon"
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+var holiday = "Easter"
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
