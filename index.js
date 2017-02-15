50bcf44095e1e39fede4b504d77e0291e1461ec0
Abdullah Alam <abdullah.alam.omi@gmail.com>
2017-02-15 02:27:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+// functions happyHolidays returns "Happy holidays!"
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+// functions happyHolidaysTo(name) returns "Happy holidays, ${name}!"
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+happyHolidaysTo('Omi')
+
+// functions happyHolidayTo(holiday, name) returns "Happy ${holiday}, ${name}!"
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+happyHolidayTo("Birthday", "Jamila")
+
+// functions holidayCountdown(holiday, days) returns "It's ${days} days until ${holiday}!"
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
+holidayCountdown("Presidents' Day", 6)
