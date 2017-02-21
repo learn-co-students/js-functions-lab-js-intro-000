206fca3a9419100334e699fd9df45a8ab2175c64
Ryan Stroh <ryan@rysen.com.au>
2017-02-21 03:34:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo() {
+  var name = "you";
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(string) {
+  var name = "you",
+      holiday = string;
+
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+
+  return `It's ${days} days until ${holiday}!`
+}
