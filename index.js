684d0ccf22cf0bef04fa6a708310f9944c16d5de
Hong Choat <hchoat1@gmail.com>
2017-03-12 00:16:00 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
+}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+function happyHolidayTo(holiday, name) {
+   return `Happy ${holiday}, ${name}!`
+ }
+function holidayCountdown(holiday, days){
+  return "It's 20 days until Mother's Day!"
+}
