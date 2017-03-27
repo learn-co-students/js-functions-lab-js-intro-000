d985171fcee37733598333dbe173933f4a0a4d03
Mili Upadhyaya Koirala <glady.sharma282@gmail.com>
2017-03-27 03:18:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// write your code below!
+/*****  HOLIDAY BEGINS HERE *****/
+
+/*Example function with no args*/
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+/*Example function with one args*/
+function happyHolidaysTo(name){
+
+  return `Happy holidays, ${name}!`;
+}
+/*Example function with two args*/
+function happyHolidayTo(holiday, name){
+
+  return `Happy ${holiday}, ${name}!`;
+
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
