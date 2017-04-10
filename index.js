58f28705303a4a4abb5a380b5410f0d2da4ada5a
Kraig Tapang <kraig.tapang@gmail.com>
2017-04-10 03:41:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+
+
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+//remember ${} is used for a placeholder
+//im more
+function happyHolidaysTo(name){
+  return "Happy holidays, " + name + "!";
+}
+
+function happyHolidayTo(holiday, name){
+  return "Happy " + holiday + ", " + name + "!";
+}
+
+//remember to use a backslask before a single apostrophe in a string
+//or else it will confuse it as an ending of quote for a string
+function holidayCountdown(holiday, days){
+  return "It\'s " + days + " days until " + holiday + "!";
+}
+
