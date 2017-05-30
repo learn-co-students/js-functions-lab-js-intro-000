e9369e6e577560fe104bf0fec56c4e942e07e116
Olguin Perdomo <olquinperdomo@me.com>
2017-05-30 14:03:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+var name = "Johnny Walker"
+var holiday = "Christmas"
+
+function happyHolidaysTo(holiday,name){
+  return "${name}";
+}
+
+function holidayCountdown(){
+  return "It's ${days} days until ${holiday}!"
+}
