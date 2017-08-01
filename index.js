5187250ff3675e17e2087b0b8c09da81808f8a44
Scotty F <scottymferreira@gmail.com>
2017-08-01 03:30:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){// this is the second lab question with template lierals.
+  return `Happy holidays, ${name}!`
+}
+//function happyHolidaysTo(you){ //this is what I got without template literals
+//  return "Happy holidays, you!"
+//}
+
+function happyHolidayTo (holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown (holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
