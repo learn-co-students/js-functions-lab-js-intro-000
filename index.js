4b246e5a85041db5294b02fd5982b7ba0074554b
Jacob Koo <gobleskoo@gmail.com>
2017-09-01 20:59:08 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
   var holiday = "Mother's Day"
-  return 'It\'s ${days} days until ${holiday}!'
+  return `It's ${days} days until ${holiday}!`
 }
