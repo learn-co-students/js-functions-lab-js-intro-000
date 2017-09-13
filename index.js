e73756870b98078bafae4ad4cf1526bfbb6dcccb
Miguel Angel Califa Urquiza <miguelangelcu@ufps.edu.co>
2017-09-13 21:52:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function  happyHolidays(string){
+  return "Happy holidays!";
+}// write your code below!
+function  happyHolidaysTo(name){
+   return "Happy holidays, "+name+"!";
+}// write your code below!
+function  happyHolidayTo(holiday,name){
+  return "Happy "+holiday+", "+name+"!";
+}// write your code below!
+function  holidayCountdown(holiday,number){
+  return "It\'s "+number+" days until "+holiday+"!";
+}
