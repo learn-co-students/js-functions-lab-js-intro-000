f8f442f23a7e23218d797f15aab7e82eaf0be800
Eric Sherman <esherman9@gmail.com>
2017-10-11 03:29:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
 };
 
 function happyHolidayTo(name, holiday){
-  return "Happy ${holiday}, ${name}!"
+  return "Happy " + holiday + ", " + name + "!"
 };
 
 function holidayCountdown(days, holiday){
