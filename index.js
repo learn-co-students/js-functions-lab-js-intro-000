54f4af595268816fc919098ccd2256ecc06c2526
Eric Sherman <esherman9@gmail.com>
2017-10-11 03:32:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name){
 };
 
 function holidayCountdown(holiday, days){
-  return "It's " + days + " until "+ holiday + "!"
+  return "It's " + days + " days until "+ holiday + "!"
 };
