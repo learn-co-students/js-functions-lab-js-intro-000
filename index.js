898c9a36b41bc0e7d3d9b48fb5e76688277b3518
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-07 02:08:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // write your code below!
+//function number one happyHolidays
+
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+//function number two happyHolidaysTo
+
+function happyHolidaysTo(sendto) {
+  return `Happy holidays, ${sendto}!`;
+}
+
+//function number three happyHolidayTo
+
+function happyHolidayTo(holiday, dear) {
+  return `Happy ${holiday}, ${dear}!`;
+}
+
+//function number four holidayCountdown
+
+function holidayCountdown(holiday, daystil) {
+  return `It's ${daystil} days until ${holiday}!`;
+}
+
+//testing in repl.it before executing here
+// all ran except
+//forgot semicolons
+//when calling a function the parameters must be a string or number
