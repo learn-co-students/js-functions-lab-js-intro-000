740c39fb92967068dcbea2271a2eef2b10191378
Nguyễn Duy Phước <phuoc.nguyen@metropolia.fi>
2017-11-16 16:57:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(day, name) {
+  return `Happy ${day}, ${name}!`
+}
+
+function holidayCountdown(day, numDays) {
+  return `It's ${numDays} days until ${day}!`
+}
