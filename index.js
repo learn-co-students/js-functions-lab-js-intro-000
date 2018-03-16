554a52120efe1c754693ad6301f12bb39a62ba54
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-16 14:27:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// write your code below!
+
+var holiday = "Happy holidays!";
+
+function happyHolidays() {
+  return holiday;
+}
+
+
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+
+function happyHolidayTo (holiday, name) {
+ return `Happy ${holiday}, ${name}!`;
+}
+
+
+function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`;
+}
