79868a3e95c124ca6a355f169fb8a6dc3ee51df4
Steven Rondeau <steve.rondeau@sentinelspc.com>
2018-04-16 16:56:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+var holiday = 'December'
+var name = 'George'
+var days = 30
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`);
+
+}
+console.log(`Happy holidays, ${name}!`)
+
+function happyHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
+	console.log(`Happy ${holiday}, ${name}!`);
+
+function holidayCountdown(holiday,days) {
+  return (`It's ${days} days until ${holiday}!`)
+}
+  console.log(`It's ${days} days until ${holiday}!`)
