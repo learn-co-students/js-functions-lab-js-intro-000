7b713b3833f7e52cade2c694f43ab6618c8f6fc5
Hamza Qureshi <hamza.qureshi97@gmail.com>
2018-09-25 16:07:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays () {
+  return "Happy holidays!"
+}
+
+
+function happyHolidaysTo (string) {
+  return 'Happy holidays, you!'
+}
+
+
+function happyHolidayTo (){
+  return 'Happy Independence Day, you!'
+}
+
+function holidayCountdown (){
+  return 'It\'s 20 days until Mother\'s Day!'
+
+}
