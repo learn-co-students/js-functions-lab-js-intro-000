f021a80c325a0517532aa80609143f50069ad1dd
brave-course-1385 <tania@pixelsandcode.studio>
2018-11-12 22:38:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(string){
+  return ('Happy holidays!')
+}
+
+function happyHolidaysTo(name){
+  return (`Happy holidays, ${name}!`)
+}
+
+
+function happyHolidaysTo(holiday, name){
+  returns ('Happy' `${holiday}, ${name}!`)
+}
