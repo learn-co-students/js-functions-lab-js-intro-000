b8b162b45c1cc7b904bedfdda4d736933074e8f3
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-03 03:48:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays(string)
+{
+return "Happy holidays!"
+}
+
+function happyHolidaysTo(Jack)
+{return `Happy holidays, ${Jack}!`
+}
+
+function happyHolidayTo(Christmas, Jack)
+{return `Happy ${Christmas}, ${Jack}!`
+}
+
+function holidayCountdown(Christmas, ten)
+{return `It\'s ${ten} days until ${Christmas}!`
+}
