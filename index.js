e8bdfd756d3b6e523a7bdb6922a410b2552eefe6
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-06 13:41:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays(){
+  
+  return "Happy holidays!";
+}
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+function happyHolidayTo(holidays, name){
+  return `Happy ${holidays}, ${name}!`;
+}
+function holidayCountdown(holidayName,NumberOfDays){
+  return `It's ${NumberOfDays} days until ${holidayName}!`;
+}
