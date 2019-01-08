9cd1f24377ca242620ae9285664cdff3ab330b0e
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-08 23:55:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(Theodore) {
+  return "Happy holidays, ${you}!"
+}
+function happyHolidayTo() {
+  return `Happy ${holiday},${Theodore}!`
+}
+function holidayCountdown(string) {
+  return `it's {monday, tuesday, wednesday,thursday} ${friday}!`
+}
