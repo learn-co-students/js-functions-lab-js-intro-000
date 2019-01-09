84278a54501ac97680055cafc212ec276a1a6624
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-09 01:07:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
-function happyHolidaysTo(parameter) {
+function happyHolidaysTo(Theodore) {
   return "Happy holidays, you!"
 }
 function happyHolidayTo(holiday,name) {
-  return &#96;"Happy ${Independence Day}, ${Theodore}!"&#96;
+  return &#96;Happy ${holiday}, ${name}!&#96;
 }
