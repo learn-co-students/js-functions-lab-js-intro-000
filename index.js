139ebbaadb1db45c5684be1947509841adc92178
Reed G <ellisonrg@gmail.com>
2019-03-02 03:35:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
+  var happyHolidaysTo;("meredith")
+}
+
+
+function happyHolidayTo(holiday, name) {
+  return "Happy $(holiday), ${name}!"
+  holiday('christmas')
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
