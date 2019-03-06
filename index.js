69d6f0cefed448ff4bba10b2ad23d5573bae1039
Fiona Comerford <fionaco720@gmail.com>
2019-03-06 22:45:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ function happyHolidays(){
 }
 function happyHolidaysTo('Fiona'){
   return `Happy Holidays, ${Fiona}!`
-  
+
+}
+function happyHolidayTo('Fiona'){
+  return (`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown('holiday', '2'){
+  return (`It's ${days} days until ${holiday}!`)
 }
