1dd807e8a696bd7948b936fb7dcc753b82b7491c
Ilya Fischhoff <fischhoff@gmail.com>
2019-03-12 22:58:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";   // The function returns the product of p1 and p2
+}
+
+//This function should accept a parameter of the name of the person you want to wish happy holidays, and return the string `Happy holidays, ${name}!`
+//function happyHolidaysTo(name) {
+//  return "Happy holidays, " + name + "!";   // The function returns the product of p1 and p2
+//}
+//using string interpolation
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;   // The function returns the product of p1 and p2
+}
+
+//Write a function named happyHolidayTo. This function should accept two parameters, the holiday you want to wish them well for, and the name of the person you're wishing well. This function should return the string `Happy ${holiday}, ${name}!`
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+//Write a function named holidayCountdown. This function should accept two parameters, the holiday name and number of days till that holiday. The function should return the string `It's ${days} days until ${holiday}!`
+function holidayCountdown(days, holiday){
+  return `It\'s ${days} days until ${holiday}!` 
+}
