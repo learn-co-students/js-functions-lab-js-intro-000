a334ca85a9d2c943f0a7206715f219c2be4e1bc0
Caroline Ninganga <carolninganga@gmail.com>
2019-04-21 03:00:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, " + name + "!";
+}
+
+happyHolidaysTo("Helga");
+
+function happyHolidayTo(holiday, name){
+  return "Happy " + holiday + ", " + name + "!"
+}
+
+happyHolidayTo("easter", "helga");
+
+function holidayCountdown(holiday, days){
+  return "It\'s " + days + " days until " + holiday +"!";
+}
+
+holidayCountdown("easter", 5)
