b9b6b396f1a536de192ed823c480fca94ef2538e
Alex East <alexeastltd@gmail.com>
2019-05-14 04:57:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays(String) {
+    return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
