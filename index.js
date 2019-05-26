aa1952245485d188e3d1197572f9ae728b08aa33
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-05-26 17:09:46 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountDown(days, holiday) {
+function holidayCountDown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
