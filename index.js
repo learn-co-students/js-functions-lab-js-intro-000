8399e8c555b7af3b7874057920b0907149fef655
Vanessa Anugwom <vanugwom@hotmail.co.uk>
2019-06-08 15:56:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // write your code below!
+function happyHolidays() {return ("Happy holidays!");}
+function happyHolidaysTo (name) {return (`Happy holidays, ${name}!`)}
+function happyHolidaysTo (name, holiday) { return (`Happy ${holiday}, ${name}!`)}
