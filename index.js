81ba746740da4cd1d936f89f32c3828ddedd1473
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-30 21:04:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+//happyHolidays();
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+
+//happyHolidaysTo("Rusty");
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+//happyHolidayTo("Summer Solstice", "Krystek");
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!` ;
+}
+
+//holidayCountdown("Gabriel's birthday", 12);
