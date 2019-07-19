7c02705a44dfccb318232521f431e974cd4d8c97
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-19 21:45:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(pers) {
+  return 'Happy holidays, ' + pers + "!"
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, time) {
+  return `It's ${time} days until ${holiday}!`
+}
