dfc4d9ed0946279e46472e0966e2bf554155cbef
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-17 03:07:14 +0000

    Done.

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ function happyHolidays() {
 
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
-  happyHolidaysTo('Tati')
 }
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
-  happyHolidayTo('BD', 'Tati')
 }
 
 function holidayCountdown (holiday, days) {
   return `It's ${days} days until ${holiday}!`
-  holidayCountdown('20', 'BD')
 }
