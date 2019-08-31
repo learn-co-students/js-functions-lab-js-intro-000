6e907b72378078346c6f78278172c3b7a54b9dc4
Aleksei Lesnikov <alekseilesnikov@gmail.com>
2019-08-31 00:22:47 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
+}
+function happyHolidayTo(IndependenceDay, Alex){
+  return `Happy ${IndependenceDay}, ${Alex}!`
+}
+function holidayCountdown(IndependenceDay, days){
+  return `It's ${days} days until ${IndependenceDay}!`
 }
