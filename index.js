94ec9a0de883538aa1f30d143806b5483b0ded92
Melike KILIC <melike180400@gmail.com>
2019-09-03 17:03:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+function happyHolidays() {
+return "Happy holidays!" ;
+}
+
+  function happyHolidaysTo(Mel){
+  return 'Happy holidays, ${name}!' ;
+
+}
+function happyHolidayTo(July 4 th, Mel){
+  return 'Happy ${holiday}, ${name}!' ;
+}
+  function holidayCountdown(71,Veterans Day){
+  return "It's ${days} days until ${holiday}!" ;
+}
