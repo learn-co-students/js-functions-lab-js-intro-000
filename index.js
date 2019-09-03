74ef40695c772b4fa8d78ee11a2f6511cbc9b703
Melike KILIC <melike180400@gmail.com>
2019-09-03 17:55:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // write your code below!
 function happyHolidays() {
-return "Happy holidays!" ;
+  return "Happy holidays!";
 }
 
-  function happyHolidaysTo(Mel){
-  return 'Happy holidays, ${name}!' ;
+function happyHolidaysTo(name){
+   return `Happy holidays, ${name}!` ;
+}
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!` ;
+}
+ function holidayCountdown(holiday,days){
+ return `It's ${days} days until ${holiday}!` ;
 
-}
-function happyHolidayTo(July 4 th, Mel){
-  return 'Happy ${holiday}, ${name}!' ;
-}
-  function holidayCountdown(71,Veterans Day){
-  return "It's ${days} days until ${holiday}!" ;
 }
