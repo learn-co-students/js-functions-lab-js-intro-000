6a13bacbcea40e3e7b6e16dec945f9ca10f6ba10
Sarah Wanjiru Irungu <sarawanjiru29@gmail.com>
2019-10-10 12:33:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+var mother = "Mother's Day";
+var days = 20;
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+
+function happyHolidaysTo(Kim) {
+  return `Happy holidays, ${Kim}!`
+}
+
+function happyHolidayTo(Independence, Kim) {
+  return `Happy ${Independence}, ${Kim}!`
+}
+function holidayCountdown(mother, days) {
+  return `It\'s ${days} days until ${mother}!`
+}
