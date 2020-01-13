081a63ad49d20b24bd469ae66ae444141eca5f1a
Oscar <mundonatural@gmail.com>
2020-01-13 14:42:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(nombre) {
+  return `Happy holidays, ${nombre}!`;
+}
+
+function happyHolidayTo(dia, nombre) {
+  return `Happy ${dia}, ${nombre}!`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`;
+}
+
 // write your code below!
