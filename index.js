0b77a110a448d6377fb0ffd8c5e3b46df9d07541
mark mosby <markemosby@msn.com>
2020-02-12 22:17:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+
+function happyHolidays(){
+return "Happy holidays!";
+}
+function happyHolidaysTo(name){
+return "Happy holidays, ${name}!";
+}
+//Write a function that takes in multiple parameters
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+//Write a function that takes in multiple parameters
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
