0ffc14637725b3dfd5e91eaad1d7e8c56ea8a972
Chana Kupetz <chanakupetz@gmail.com>
2020-02-20 14:34:36 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
-  return `It's ${holiday} days until ${days}!`
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
