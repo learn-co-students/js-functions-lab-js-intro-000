d7e3999a1519f23a92df78d2bfdaa9ceded4ae2b
Yann Xiaoyan Yang <yannxyang@sina.com>
2020-02-28 21:37:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// write your code below!
+function happyHolidays() {
+    return "Happy holidays!";}
+
+
+happyHolidaysTo('Yann')
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+    }
+
+happyHolidaysTo("Labor Day",'Yann')
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+holidayCountdown("3","Labor Day")
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`;
+}
