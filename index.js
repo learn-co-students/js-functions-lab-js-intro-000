c5a77a3916b25d7029b8d12d0540d67956a4691c
moataz osama <moataz.osama@hotmail.com>
2020-03-15 08:54:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+//var = happyHolidays
+function happyHolidays()
+ {return "Happy holidays!";}
+function happyHolidaysTo(moataz)
+{return `Happy holidays, ${moataz}!`}
+function happyCustomHolidayTo( July ,moataz)
+{return `Happy ${July }, ${moataz}!`}
+function holidayCountdown( July,ten)
+{return`It's ${ten} days until ${July}!`}
