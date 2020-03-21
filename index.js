3992f90a7ddcc174ca7beed5cd70f20b248eedd2
Kevin Minutti <minutti70@gmail.com>
2020-03-21 02:26:50 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyCustomHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
