0a5c5e72779baeacea61fa5a056f95b22a279abd
Unique Mark Michael <uniquemarkmichael@gmail.com>
2020-03-23 16:34:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays(string){
+return "Happy holidays!";
+}
+
+function happyHolidaysTo(everyone){
+  return (`Happy holidays, ${everyone}!`)
+}
+
+
+function happyCustomHolidayTo(holiday, name){
+return (`Happy ${holiday}, ${name}!`)
+
+}
+
+function holidayCountdown(holiday, days){
+return (`It's ${days} days until ${holiday}!`)
+
+}
