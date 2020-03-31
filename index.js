9fe777e2d2574ad8a3200cc709675b720776882f
Terron Jackson <terronjackson18@gmail.com>
2020-03-31 23:11:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(Raeven) {
+  return `Happy holidays, ${Raeven}!`
+}
+
+function happyCustomHolidayTo(holiday, Raeven) {
+  return `Happy ${holiday}, ${Raeven}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
