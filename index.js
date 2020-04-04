abcfd4ff55b3645c31a06801ea86bd6b318b1766
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-04 14:03:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+//happyHolidays should not accept any parameters and return the string "Happy holidays!"
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+
+//happyHolidaysTo with name parameter
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+//happyCustomHolidayTo parameters pass holiday, then name
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+//holidayCountdown parameters pass number of days until holiday
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
+}
