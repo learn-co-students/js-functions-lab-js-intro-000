b0660951a87ad365dcc96e30bee31e0e4d0b34cd
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-05 04:27:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+console.log(happyHolidays());
+
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+console.log(happyHolidaysTo('hoang'));
+
+function happyCustomHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+console.log(happyCustomHolidayTo("New Year","Hoang"));
+
+
+function holidayCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`;
+}
+console.log(holidayCountdown("Chrismas",45));
