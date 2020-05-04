0478c85c7bbb276589560f811e6a3d00df0862c5
Brett Wilson <wilson.3458@osu.edu>
2020-05-04 06:55:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+// Returns string "Happy Holidays!"
+function happyHolidays(){
+  return "Happy holidays!";
+}
+// Returns a string including input name.
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+// Returns a string including input holiday & name.
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+// Returns a countdown until an input holiday.
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
