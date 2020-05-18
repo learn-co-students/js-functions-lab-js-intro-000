9b022f347c1239a25623c34387ea857d8057741f
Ismaiilov Atai <atai2497@gmail.com>
2020-05-18 17:23:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays() {
+  var what = "Happy holidays!"
+  return what
+}
+happyHolidays();
+
+function happyHolidaysTo(name) {
+return `Happy holidays, ${name}!`;
+}
+happyHolidaysTo("you");
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+happyCustomHolidayTo("NewYear", " Tenirlan")
+
+function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`;
+}
+holidayCountdown(" Mother's day", 20);
