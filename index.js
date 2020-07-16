7f00a198ca6a8167e85b4e4cfc45296a8ca3c066
Kamil <kovval78@gmail.com>
2020-07-16 18:16:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  if (name === "Kamil") {
+    return `Happy holidays, ${name}!`;
+  }
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  if (holiday === "Niedziela" && name === "Kamil") {
+    return `Happy ${holiday}, ${name}!`;
+  }
+}
+
+function holidayCountdown(holiday, numbers) {
+  if (holiday === "Niedziela" && days === 7) {
+    return `It's ${days} days until ${holiday}!`
+  }
+}
