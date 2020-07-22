b813ce0f5cdcb00959eca39da38c739aa8d69a18
Cole Cezeaux <colecezeaux@gmail.com>
2020-07-22 15:00:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+// write your code below!
+function happyHolidays(string) { //This function should not accept any parameters
+  return "Happy holidays!" //return the string "Happy holidays!"
+}
+function happyHolidaysTo(name) { //This function should accept a parameter of the name of the person you want to wish happy holidays
+  return (`Happy holidays, ${name}!`) //return the string `Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday) { //This function should accept two parameters, the holiday you want to wish them well for, and the name of the person you're wishing well
+  return (`Happy ${holiday} ${name} !`) // return the string `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days) { //This function should accept two parameters, the holiday name and number of days until that holiday
+  return (`It/s ${days} days until ${holiday}!`) //return the string `It's ${days} days until ${holiday}!`
+}
