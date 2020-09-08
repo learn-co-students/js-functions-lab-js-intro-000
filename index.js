50749c297a97b2ccc4b24d5c3929970d85e19d9b
Luca Iacob <luca.iacob.d@gmail.com>
2020-09-08 13:03:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+var date = "October 18th"
+console.log('My birthday is ' + date);
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
