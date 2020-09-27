50d29bc2be5fe9ddc4a0a0b2ce085b2c4424edfd
David Glading <davidjglading@gmail.com>
2020-09-27 22:13:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 // writer code below!
-functions "happyHolidays()"  {
+function happyHolidays()  {
   return "Happy holidays!"
-  console.log("Happy holidays!")
 }
 
 function happyHolidaysTo(name)  {
-  var name = "you"
+  var name1 = "everyone"
+  var name2 = "you"
   return(`Happy holidays, ${name}!`)
-  console.log("Happy holidays, you!"
+  expect(`Happy holidays, ${name1}!`).toEqual("Happy holidays, everyone!")
+  expect(`Happy holidays, ${name2}!`).toEqual("Happy holidays, you!")
 }
 
 function happyCustomHolidayTo(holiday, name)  {
-  var holiday = "Independence Day"
-  var name = "you"
-  console.log(`Happy ${holiday}, ${name}`)
-  return "Happy Independence Day, you"
+  var holiday1 = "Independence Day"
+  var holiday2 = "New Year"
+  var name1 = "you"
+  var name2 = "everyone"
+  return(`Happy ${holiday}, ${name}!`)
+  expect(`Happy ${holiday1}, ${name1}!`).toEqual("Happy Independence Day, you!")
+  expect(`Happy ${holiday2}, ${name2}!`).toEqual("Happy New Year, everyone!")
 }
 
 function holidayCountdown(holiday, days)  {
-  var holiday = "Mother's Day"
-  var days = "20"
-  console.log(`It's ${days} days until ${holiday}!`)
-  return "It's 20 days until Mother's Day!"
+  var holiday1 = "Mother's Day"
+  var holiday2 = "Father's Day"
+  var days1 = "20"
+  var days2 = "120"
+  return(`It's ${days} days until ${holiday}!`)
+  expect(`It's ${days1} days until ${holiday1}!`).toEqual("It's 20 days until Mother's Day!")
+  expect(`It's ${days2} days until ${holiday2}!`).toEqual("It's 120 days until Father's Day!")  
 }
