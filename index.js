e0657d86198cf4cc8ca9d11822358f99c8f197a7
David Glading <davidjglading@gmail.com>
2020-09-27 20:21:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// write your code below!
+// writer code below!
+functions "happyHolidays()"  {
+  return "Happy holidays!"
+  console.log("Happy holidays!")
+}
+
+function happyHolidaysTo(name)  {
+  var name = "you"
+  return(`Happy holidays, ${name}!`)
+  console.log("Happy holidays, you!"
+}
+
+function happyCustomHolidayTo(holiday, name)  {
+  var holiday = "Independence Day"
+  var name = "you"
+  console.log(`Happy ${holiday}, ${name}`)
+  return "Happy Independence Day, you"
+}
+
+function holidayCountdown(holiday, days)  {
+  var holiday = "Mother's Day"
+  var days = "20"
+  console.log(`It's ${days} days until ${holiday}!`)
+  return "It's 20 days until Mother's Day!"
+}
