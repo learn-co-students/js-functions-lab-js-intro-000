9f7ab4079ed02c7d0ab13e07e5eb3fa5573e778d
Garrett Breach <breachg1992@gmail.com>
2020-10-19 17:52:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code below!
+function happyHolidays() {
+  return ("Happy holidays!")
+}// write your code below!
+
+var name = "You"
+var holiday = "Halloween"
+var days = 12
+
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`)
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(holiday, days) {
+  return (`It's ${days} days until ${holiday}!`)
+}
