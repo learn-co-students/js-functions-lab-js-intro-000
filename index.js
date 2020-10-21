227e70e51f0036b7b40f881a95d364ecf88ca26e
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-21 04:43:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+const name = "you";
+const holiday = "Mother's Day";
+const days = "20";
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
