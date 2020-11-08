d90ef88d261bde2c319a837ad3a4fec7955aeeb2
inherited-checkpoint-3530 <webdevcarlosferreira@gmail.com>
2020-11-08 23:52:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+var happyHolidays = function happyHolidays(){
+  return 'Happy holidays!';
+}
+happyHolidays();
+var happyHolidaysTo = function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`;
+}
+happyHolidaysTo('Carlos');
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`;
+}
+happyCustomHolidayTo('Halloween','Carlos');
+function holidayCountdown(holiday,days){
+  return`It's ${days} days until ${holiday}!`;
+}
+holidayCountdown('Halloween',20);
