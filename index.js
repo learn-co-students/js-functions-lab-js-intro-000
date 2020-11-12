a03d643d0c0bcc28c67f6251de1f353344a1bcc7
Dilovar Mamadov <d.mamadov@gmail.com>
2020-11-12 03:45:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+happyHolidaysTo('Dilovar')
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+happyCustomHolidayTo('Eid', 'Dilovar')
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
+holidayCountdown(5, 'Eid')
