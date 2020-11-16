29452b6fba86040dedfc2f845f6ccac0a823d7fa
Amy Simpson <simpson.r.amy@gmail.com>
2020-11-16 22:06:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// // write your code below!
-// function happyHolidays() {
-//   return "Happy holidays!"
-// }
-
-// function happyHolidaysTo(name) {
-//   return `Happy holidays, ${name}!`
-// }
-
-// function happyCustomHolidayTo(holiday, name) {
-//   return `Happy ${holiday}, ${name}!`
-// }
-
-// function holidayCountdown(holiday, days) {
-//   return `It's ${days} days until ${holiday}!`
-// }
-
 function happyHolidays() {
 	return "Happy holidays!";
 }
