2e8d389b6feea92091726755a466aab61c80441e
Wesley B. Howard <bryce9643@gmail.com>
2021-02-07 00:59:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+
+
+var name = "Wes"
+var holiday = "Christmas"
+var days = "10"
+
+function happyHolidays() {
+  return ('Happy holidays!')
+}
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`)
+}
+function happyCustomHolidayTo(holiday, name) {
+    return (`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(holiday, days) {
+    return (`It's ${days} days until ${holiday}!`)
+}
