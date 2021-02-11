426b7d85b3d0eb2eaf2f3476dd84039f79d143a0
Blake Busby <blakebusby16@gmail.com>
2021-02-11 00:31:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+var name = "everyone"
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+var holiday = "whatever day"
+var name = "pal"
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+var days ="22"
+var holiday ="Meatball day"
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
