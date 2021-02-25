c7f88f2cf19877cc653f4a3ccd0c24ce492def04
Kendall George <kendallanngeorge@gmail.com>
2021-02-25 20:22:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!";
+}
+
+var name = "you"
+function happyHolidaysTo(name) {
+  return(`Happy holidays, ${name}!`);
+}
+
+var holiday = "Hanukkah";
+var name = "Janet";
+function happyCustomHolidayTo(holiday, name) {
+  return(`Happy ${holiday}, ${name}!`);
+}
+
+var holiday = "Mother's Day";
+var days = "20";
+function holidayCountdown(holiday, days) {
+  return(`It's ${days} days until ${holiday}!`);
+}
