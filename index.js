ece23fdfd5a0554ec1a23135970d6df5daeeb389
Roxana Sulton <roxsulton@icloud.com>
2021-03-06 20:01:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+
+function happyHolidaysTo(Sean) {
+  return `Happy holidays, ${Sean}!`
+}
+
+function happyCustomHolidayTo(holiday, Sean) {
+  return `Happy ${holiday}, ${Sean}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
