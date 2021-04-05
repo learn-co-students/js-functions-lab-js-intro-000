c44136de33c1775055251a00ed232aad37a168d5
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-05 12:32:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+let name='you';
+let holiday='Independence Day';
+let number='20'
+let mother="Mother's Day"
+
+function happyHolidays(string){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return`Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(mother,number){
+  return `It's ${number} days until ${mother}!`
+}
