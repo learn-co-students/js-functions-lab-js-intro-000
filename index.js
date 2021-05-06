5f9738691237085b6bad081beae08c91e6d731ef
Diego Domínguez Melo <diego_14melo@hotmail.com>
2021-05-06 04:44:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+
+function happyHolidays() {
+   return "Happy holidays!"
+}
+const happyHolidays = () => "Happy holidays!"
+
+const happyHolidaysTo = name => `Happy holidays, ${name}`
+
+const happyCustomHolidayTo = (holiday, name) => "Happy " + holiday + " , " + name + "!"
+
+const holidayCountdown = (holiday, days) => days + " days until " + holiday + "!"
